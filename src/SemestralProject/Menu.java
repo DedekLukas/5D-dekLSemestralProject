34e66e33edb87b125e9cb292bb186392245455d7
DedekLukas <115634224+DedekLukas@users.noreply.github.com>
2023-01-09 15:45:42 +0100

    Update Menu.java

--- src/SemestralProject/Menu.java
+++ src/SemestralProject/Menu.java
@@ -1,4 +1,3 @@
-
 package SemestralProject;
 
 import java.util.Scanner;
@@ -9,25 +8,21 @@ public class Menu {
         System.out.println("Semestrální práce byla spuštěna" + "\n" + "Vyber si jakou úlohu chceš spustit: ");
         Scanner sc = new Scanner(System.in);
         int choise;
-        boolean end;
         boolean znova = true;
         do {
             System.out.println("1: Semestralni uloha" + "\n" + "2: Vanocni uloha" + "\n" + "0: konec");
             choise = sc.nextInt();
-            do {
-                end = true;
                 switch (choise) {
                     case 1:
-                        SemestralniUloha.main(args); break;
+                        SemestralniUloha.Semestral(); break;
                     case 2:
-                        VanocniUloha.main(args); break;
-                    case 0 :
-                        end = true; znova = false; break;
+                        VanocniUloha.run(); break;
+                    case 0:
+                        znova = false; break;
                     default:
                         System.out.println("Neplatna volba");
-                        end = false; break;
+                        break;
                 }
-            } while (!end);
         } while (znova);
     }
 }
